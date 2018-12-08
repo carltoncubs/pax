a6b72d68d1cce94493e12a2900c1d67c8c0b0e78
Nick Spain <nicholas.spain96@gmail.com>
2018-12-08 13:09:10 +1100

    Create base form class

---
+++ src/BaseForm.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Header from "./Header.js";
+
+export default class BaseForm extends Component {
+  render() {
+    return (
+      <div>
+        <Header
+          title={this.props.title}
+          user={this.props.name}
+          email={this.props.email}
+          auth={this.props.auth}
+        />
+
+        <form
+          style={{ paddingTop: 20, paddingLeft: 200, paddingRight: 200 }}
+          autoComplete={this.props.autocomplete}
+          noValidate={this.noValidate}
+          onSubmit={this.props.onSubmit}
+        >
+          {this.props.children}
+        </form>
+      </div>
+    );
+  }
+}
