cbf3364c079aeb5369b2451147b208ba41ab4624
Nick Spain <nicholas.spain96@gmail.com>
2018-12-08 13:08:33 +1100

    Clean up imports

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 import amber from "@material-ui/core/colors/";
 import { SnackbarProvider } from "notistack";
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import { GoogleLogin } from "react-google-login";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import yellow from "@material-ui/core/colors/yellow";
 import "typeface-roboto";
 
 import App from "./App";
-import Privacy from "./Privacy";
-import Root from "./Root";
-import Settings from "./Settings";
-import SignInForm from "./SignInForm";
-import SignOutForm from "./SignOutForm";
-import config from "./config.json";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "./App.css";
 
