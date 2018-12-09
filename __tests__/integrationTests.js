8a688ea379ab4e84ed8b35360c8e32730a02880b
Nick Spain <nicholas.spain96@gmail.com>
2018-12-09 23:12:31 +1100

    Integration test stubs

---
+++ __tests__/integrationTests.js
@@ -0,0 +1,37 @@
+it("redirects all pages to / for authentication if they don't have a token", () => {
+  expect(false).toEqual(true);
+});
+
+it("redirects the user to their previous page once authenticated if there is one", () => {
+  expect(false).toEqual(true);
+});
+
+it("redirects the user to /sign-in if there is not a previous page", () => {
+  expect(false).toEqual(true);
+});
+
+it("displays a banner if the user is authenticated by google but not invited", () => {
+  expect(false).toEqual(true);
+});
+
+it(
+  "opens a drop down menu displaying logged in user's name, email and" +
+    " logout button when pressing auth circle",
+  () => {
+    expect(false).toEqual(true);
+  }
+);
+
+describe("side menu", () => {
+  it("opens on the LHS when the hamburger button is pressed", () => {
+    expect(false).toEqual(true);
+  });
+
+  it("contains links to all the different pages", () => {
+    expect(false).toEqual(true);
+  });
+
+  it("highlights the current page", () => {
+    expect(false).toEqual(true);
+  });
+});
