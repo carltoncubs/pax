bf9a85cbf788ff20fb7ec1c3a8dee8fb17ec01f7
Nick Spain <nicholas.spain96@gmail.com>
2018-12-11 17:26:01 +1100

    Add script for running all tests

---
+++ __tests__/run_tests.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+to_run="${1:-all}"
+
+it_exit_code=0
+ut_exit_code=0
+
+# Run integration tests
+if [[ "$to_run" == "integration" || "$to_run" == "all" ]]; then
+    npm start &>/dev/null &
+    server_pid=$!
+    pipenv run python integrationTests.py
+    it_exit_code=$?
+    kill $server_pid
+fi
+
+# Run unit tests
+if [[ "$to_run" == "unit" || "$to_run" == "all" ]]; then
+    npm test
+    ut_exit_code=$?
+fi
+
+exit $($it_exit_code || $ut_exit_code)
