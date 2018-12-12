5e0024903bed18e72c2a7e57eff8c9a6c1eb39a4
Nick Spain <nicholas.spain96@gmail.com>
2018-12-12 11:30:17 +1100

    Make start script respect already set envionment variables

--- scripts/start.js
+++ scripts/start.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'development';
-process.env.NODE_ENV = 'development';
+process.env.BABEL_ENV = !!process.env.BABEL_ENV ? process.env.BABEL_ENV : 'development';
+process.env.NODE_ENV = !!process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
@@ -13,7 +13,6 @@ process.on('unhandledRejection', err => {
 
 // Ensure environment variables are read.
 require('../config/env');
-
 
 const fs = require('fs');
 const chalk = require('chalk');
@@ -97,7 +96,7 @@ checkBrowsers(paths.appPath, isInteractive)
       if (isInteractive) {
         clearConsole();
       }
-      console.log(chalk.cyan('Starting the development server...\n'));
+      console.log(chalk.cyan(`Starting the ${process.env.NODE_ENV} server...\n`));
       // openBrowser(urls.localUrlForBrowser);
     });
 
