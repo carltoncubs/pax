5e46452b904d121bd4f363287468413640f5a851
Nick Spain <nicholas.spain96@gmail.com>
2018-12-13 00:18:54 +1100

    Add classes to error and success snackbars

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from "react-dom";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import yellow from "@material-ui/core/colors/yellow";
 import "typeface-roboto";
+import Button from "@material-ui/core/Button";
 
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
@@ -23,7 +24,14 @@ const theme = createMuiTheme({
 
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
-    <SnackbarProvider maxSnack={10}>
+    <SnackbarProvider
+      maxSnack={10}
+      action={[<Button size="small">Dismiss</Button>]}
+      classes={{
+        variantError: "error-notification",
+        variantSuccess: "success-notification"
+      }}
+    >
       <App />
     </SnackbarProvider>
   </MuiThemeProvider>,
