6d20ddd7225f5ae5c9fd42d0a054716c0eab1560
Nick Spain <nicholas.spain96@gmail.com>
2018-12-13 23:21:39 +1100

    Fixup return code and command starting test server

--- __tests__/run_tests.sh
+++ __tests__/run_tests.sh
@@ -7,7 +7,7 @@ ut_exit_code=0
 
 # Run integration tests
 if [[ "$to_run" == "integration" || "$to_run" == "all" ]]; then
-    npm start:test &>/dev/null &
+    npm run start:test &#>/dev/null &
     server_pid=$!
     pipenv run python integrationTests.py
     it_exit_code=$?
@@ -20,4 +20,8 @@ if [[ "$to_run" == "unit" || "$to_run" == "all" ]]; then
     ut_exit_code=$?
 fi
 
-exit $($it_exit_code || $ut_exit_code)
+if [[ $it_exit_code != 0 || $ut_exit_code != 0 ]]; then
+	exit 1
+else
+	exit 0
+fi
